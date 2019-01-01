7efa4d75c539367804a9eebf6d83a92d62add472
tuyen_pham <tuyenpd@viettel.com.vn>
2019-01-01 07:54:21 +0700

    Initial commit

---
+++ app/src/main/java/joyfunee/procounter/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package joyfunee.procounter.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import joyfunee.procounter.R
+
+class MainActivity : AppCompatActivity() {
+
+    // Dem tuan, ngay, gio,phut giay, buoc chan
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/joyfunee/procounter/receiver/DateChangeReceiver.kt
@@ -0,0 +1,11 @@
+package joyfunee.procounter.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DateChangeReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
